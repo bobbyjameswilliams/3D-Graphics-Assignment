07e5deaab38043afddad367007ddef390ad0b21c
Bobby Williams <bobby.james.williams@outlook.com>
2021-11-15 16:06:35 +0000

    Working on adding window indeces

--- WindowedWall.java
+++ WindowedWall.java
@@ -1,4 +1,4 @@
-public final class TwoTriangles {
+public final class WindowedWall {
   
   // ***************************************************
   /* THE DATA
@@ -8,12 +8,29 @@ public final class TwoTriangles {
     -0.5f, 0.0f, -0.5f,  0.0f, 1.0f, 0.0f,  0.0f, 1.0f,  // top left
     -0.5f, 0.0f,  0.5f,  0.0f, 1.0f, 0.0f,  0.0f, 0.0f,  // bottom left
      0.5f, 0.0f,  0.5f,  0.0f, 1.0f, 0.0f,  1.0f, 0.0f,  // bottom right
-     0.5f, 0.0f, -0.5f,  0.0f, 1.0f, 0.0f,  1.0f, 1.0f   // top right
+     0.5f, 0.0f, -0.5f,  0.0f, 1.0f, 0.0f,  1.0f, 1.0f,  // top right
+
+    // Extra Vertices for creating window
+    -0.25f, 0.0f, -0.5f,  0.0f, 1.0f, 0.0f,  0.0f, 0.0f,  // top left
+    -0.25f, 0.0f,  0.5f,  0.0f, 1.0f, 0.0f,  0.0f, 0.0f,  // bottom left
+    0.25f, 0.0f,  0.5f,  0.0f, 1.0f, 0.0f,  0.0f, 0.0f,  // bottom right
+    0.25f, 0.0f, -0.5f,  0.0f, 1.0f, 0.0f,  0.0f, 0.0f,  // top right
+
+    // Window Vertices
+    -0.25f, 0.0f, -0.25f,  0.0f, 1.0f, 0.0f,  0.0f, 0.0f,  // top left window
+    -0.25f, 0.0f,  0.25f,  0.0f, 1.0f, 0.0f,  0.0f, 0.0f,  // bottom left window
+     0.25f, 0.0f,  0.25f,  0.0f, 1.0f, 0.0f,  0.0f, 0.0f,  // bottom right window
+     0.25f, 0.0f, -0.25f,  0.0f, 1.0f, 0.0f,  0.0f, 0.0f  // top right window
   };
   
   public static final int[] indices = {         // Note that we start from 0!
-      0, 1, 2,
-      0, 2, 3
+    0,4,5,
+    0,1,5,
+    4,8,7,
+    7,11,8,
+    
+
+
   };
 
 }
