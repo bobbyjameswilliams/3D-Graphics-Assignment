d4a1d4d8f329f5d498fb6ee625a5b511f55e8203
Bobby Williams <bobby.james.williams@outlook.com>
2021-12-04 19:59:36 +0000

    Robot moved into robot class

--- Museum_GLEventListener.java
+++ Museum_GLEventListener.java
@@ -65,6 +65,10 @@ public class Museum_GLEventListener implements GLEventListener {
    
   private boolean animation = false;
   private double savedTime = 0;
+  private TransformNode eggMoveTranslate;
+  private TransformNode phoneMoveTranslate;
+  private TransformNode lampMoveTranslate;
+  private Robot robot;
    
   public void startAnimation() {
     animation = true;
@@ -77,52 +81,6 @@ public class Museum_GLEventListener implements GLEventListener {
     savedTime = elapsedTime;
   }
 
-  public void pose1(){
-    updateMove(-4f,-20f,0);
-  }
-
-  public void pose2(){
-    updateMove(7f,-15f,90f);
-  }
-
-  public void pose3(){
-    updateMove(8f,5f,90f);
-  }
-
-  public void pose4(){
-    updateMove(0f,12f,180f);
-  }
-
-  public void pose5(){
-    updateMove(-15f,0f, -90f);
-  }
- 
-  private void updateMove(float x,float z) {
-    robotMoveTranslate.setTransform(Mat4Transform.translate(x,0,z));
-    robotMoveTranslate.update();
-    xPosition = x;
-    zPosition = z;
-  }
-
-  private void updateMove(float x,float z, float d) {
-    //robotMoveTranslate.setTransform(Mat4Transform.translate(0,0,0));
-    //robotMoveTranslate.setTransform(Mat4Transform.rotateAroundZ(-rotation));
-    Mat4 m = new Mat4(1);
-    m = Mat4.multiply(m, Mat4Transform.translate(x,0,z));
-    m = Mat4.multiply(m, Mat4Transform.rotateAroundY(d));
-    //TransformNode transform = new TransformNode("leftarm scale", m);
-
-
-    robotMoveTranslate.setTransform(m);
-    robotMoveTranslate.update();
-    xPosition = x;
-    zPosition = z;
-    rotation = d;
-  }
-
-  private void updatePose(){
-
-  }
 
   public void outsideNight(){
     windowView.getMaterial().setAmbient(0.1f,0.1f,0.1f);
@@ -137,18 +95,18 @@ public class Museum_GLEventListener implements GLEventListener {
 
   public void loweredArms() {
     stopAnimation();
-    leftFeelerRotate.setTransform(Mat4Transform.rotateAroundX(180));
-    leftFeelerRotate.update();
-    rightFeelerRotate.setTransform(Mat4Transform.rotateAroundX(180));
-    rightFeelerRotate.update();
+    robot.leftFeelerRotate.setTransform(Mat4Transform.rotateAroundX(180));
+    robot.leftFeelerRotate.update();
+    robot.rightFeelerRotate.setTransform(Mat4Transform.rotateAroundX(180));
+    robot.rightFeelerRotate.update();
   }
    
   public void raisedArms() {
     stopAnimation();
-    leftFeelerRotate.setTransform(Mat4Transform.rotateAroundX(0));
-    leftFeelerRotate.update();
-    rightFeelerRotate.setTransform(Mat4Transform.rotateAroundX(0));
-    rightFeelerRotate.update();
+    robot.leftFeelerRotate.setTransform(Mat4Transform.rotateAroundX(0));
+    robot.leftFeelerRotate.update();
+    robot.rightFeelerRotate.setTransform(Mat4Transform.rotateAroundX(0));
+    robot.rightFeelerRotate.update();
   }
 
   public void turnOffMainLights() {
@@ -177,24 +135,14 @@ public class Museum_GLEventListener implements GLEventListener {
 
   // Roots
 
-  private SGNode robotRoot;
   private SGNode eggRoot;
   private SGNode phoneRoot;
   private SGNode lampRoot;
 
-  private float xPosition;
-  private float zPosition;
   private float rotation = 0;
 
   private TransformNode translateX;
-  private TransformNode robotMoveTranslate;
-  private TransformNode eggMoveTranslate;
-  private TransformNode phoneMoveTranslate;
-  private TransformNode lampMoveTranslate;
-  private TransformNode leftFeelerRotate;
-  private TransformNode rightFeelerRotate;
-  private TransformNode headRotate;
-  private TransformNode bodyRotate;
+
 
   private TransformNode lampRotate;
   
@@ -278,151 +226,24 @@ public class Museum_GLEventListener implements GLEventListener {
     return new Model(gl, camera, lights, shader, material, modelMatrix, mesh, texture1, texture2);
   }
 
-  // SCENES
+  public void pose1(){
+    robot.pose1();
+  }
 
-  private void robot_scene(GL3 gl){
+  public void pose2(){
+    robot.pose2();
+  }
 
-    float bodyScaleFactor = 5f;
-    float footScaleFactor = 2f;
-    float headScaleFactor = 2f;
-    float eyeScaleFactor = 0.5f;
-    float feelerScaleFactor = 0.3f;
-    float neckScaleFactor = bodyScaleFactor / 8;
+  public void pose3(){
+    robot.pose3();
+  }
 
-    float footHeight =  footScaleFactor / 2 ;
-    float bodyHeight = footHeight + footScaleFactor / 2 + bodyScaleFactor/2 ;
-    float neckHeight =  bodyHeight + bodyScaleFactor / 2 - (footHeight * 2);
-    float headHeight =  neckScaleFactor / 2 + headScaleFactor / 2;
-    float eyeHeight = headHeight + headScaleFactor/10 ;
-    float feelerHeight = headHeight + (headScaleFactor / 2);
+  public void pose4(){
+    robot.pose4();
+  }
 
-    float leftFeelerStartAngle = -30;
-    float rightFeelerStartAngle = 30;
-    float footBodyAboutFootStartAngle = 30;
-    float footBodyArticulateStartAngle = 0;
-    float neckHeadStartangle = 30;
-    //
-    robotRoot = new NameNode("root");
-    robotMoveTranslate = new
-            TransformNode("robot transform",Mat4Transform.translate(xPosition,0,zPosition));
-    TransformNode robotTranslate = new
-            TransformNode("robot transform",Mat4Transform.translate(0,0,0)); //-17f
-
-    //Foot
-    NameNode foot = new NameNode ("foot");
-    //TransformNode footTranslate = new TransformNode("foot translate", Mat4Transform.translate(0, footHeight,0));
-    Mat4 m = new Mat4(1);
-    m = Mat4.multiply(m,Mat4Transform.translate(0,footHeight,0));
-    m = Mat4.multiply(m, Mat4Transform.scale(footScaleFactor,footScaleFactor,footScaleFactor));
-    TransformNode footTransform = new TransformNode("head transform", m);
-    ModelNode footShape = new ModelNode("Sphere(foot)", sphere);
-
-    //Body needs a rotate node
-    NameNode body = new NameNode("body");
-    TransformNode bodyTranslate = new TransformNode("body translate",
-            Mat4Transform.translate(0,footHeight * 2,0));
-    m = Mat4Transform.rotateAroundX(0);
-    bodyRotate = new TransformNode("body rotate",Mat4.multiply(m,Mat4Transform.rotateAroundZ(footBodyAboutFootStartAngle)));
-
-    m = Mat4Transform.translate(0,bodyHeight / 2,0);
-    m = Mat4.multiply(m, Mat4Transform.scale(bodyScaleFactor/2,bodyScaleFactor,bodyScaleFactor/2));
-    TransformNode bodyScale = new TransformNode("head transform", m);
-    ModelNode bodyShape = new ModelNode("Sphere(body)", sphere);
-
-    //Neck
-    NameNode neck = new NameNode("neck");
-    m = Mat4Transform.translate(0,neckHeight,0);
-    m = Mat4.multiply(m, Mat4Transform.scale(neckScaleFactor, neckScaleFactor, neckScaleFactor));
-    TransformNode neckTransform = new TransformNode("head transform", m);
-    ModelNode neckShape = new ModelNode("Sphere(body)", sphere);
-
-    //Head needs a rotate node (between it and neck)
-    //Head
-
-    NameNode head = new NameNode("head");
-    TransformNode headTranslate = new TransformNode("body translate",
-            Mat4Transform.translate(0,neckHeight,0));
-    m = Mat4Transform.rotateAroundX(0);
-    headRotate = new TransformNode("head rotate",Mat4.multiply(m,Mat4Transform.rotateAroundZ(neckHeadStartangle)));
-    m = Mat4Transform.translate(0,headHeight,0);
-    m = Mat4.multiply(m, Mat4Transform.scale(headScaleFactor,headScaleFactor ,headScaleFactor));
-    TransformNode headScale = new TransformNode("head transform", m);
-    ModelNode headShape = new ModelNode("Sphere(head)", cube);
-
-    //Left eye
-    NameNode leftEye = new NameNode("leftEye");
-    m = new Mat4(1);
-    m = Mat4.multiply(m,Mat4Transform.translate(-(headScaleFactor/3),eyeHeight,(headScaleFactor/2)));
-    m = Mat4.multiply(m, Mat4Transform.scale(eyeScaleFactor, eyeScaleFactor, eyeScaleFactor));
-    TransformNode leftEyeTransform = new TransformNode("head transform", m);
-    ModelNode leftEyeShape = new ModelNode("Sphere(eye)", eye);
-
-    //Right eye
-    NameNode rightEye = new NameNode("rightEye");
-    m = new Mat4(1);
-    m = Mat4.multiply(m,Mat4Transform.translate((headScaleFactor/3),eyeHeight,(headScaleFactor/2)));
-    m = Mat4.multiply(m, Mat4Transform.scale(eyeScaleFactor, eyeScaleFactor, eyeScaleFactor));
-    TransformNode rightEyeTransform = new TransformNode("head transform", m);
-    ModelNode rightEyeShape = new ModelNode("Sphere(eye)", eye);
-
-    //Right Feeler
-    NameNode rightFeeler = new NameNode("right arm");
-    TransformNode rightFeelerTranslate = new TransformNode("right feeler translate",
-            Mat4Transform.translate(-(headScaleFactor/2),feelerHeight,0));
-    rightFeelerRotate = new TransformNode("rightarm rotate",Mat4Transform.rotateAroundZ(rightFeelerStartAngle));
-
-    m = new Mat4(1);
-    m = Mat4.multiply(m, Mat4Transform.scale(feelerScaleFactor,feelerScaleFactor * 10,feelerScaleFactor));
-    m = Mat4.multiply(m, Mat4Transform.translate(0,0.5f,0));
-    TransformNode rightFeelerScale = new TransformNode("right feeler scale", m);
-    ModelNode rightArmShape = new ModelNode("Cube(right feeler)", cube);
-
-    //Left Feeler
-    NameNode leftFeeler = new NameNode("left arm");
-    TransformNode leftFeelerTranslate = new TransformNode("left feeler translate",
-            Mat4Transform.translate((headScaleFactor/2),feelerHeight,0));
-    leftFeelerRotate = new TransformNode("leftfeeler rotate",Mat4Transform.rotateAroundZ(leftFeelerStartAngle));
-
-    m = new Mat4(1);
-    m = Mat4.multiply(m, Mat4Transform.scale(feelerScaleFactor,feelerScaleFactor * 10,feelerScaleFactor));
-    m = Mat4.multiply(m, Mat4Transform.translate(0,0.5f,0));
-    TransformNode leftFeelerScale = new TransformNode("leftarm scale", m);
-    ModelNode leftFeelerShape = new ModelNode("Cube(left arm)", cube);
-
-    //Robot Scene Graph
-    robotRoot.addChild(robotMoveTranslate);
-      robotMoveTranslate.addChild(robotTranslate);
-        robotTranslate.addChild(foot);
-          foot.addChild(footTransform);
-            footTransform.addChild(footShape);
-              foot.addChild(body);
-                body.addChild(bodyTranslate);
-                bodyTranslate.addChild(bodyRotate);
-                  bodyRotate.addChild(neck);
-                  neck.addChild(neckTransform);
-                    neckTransform.addChild(neckShape);
-                bodyRotate.addChild(bodyScale);
-                    bodyScale.addChild(bodyShape);
-                  neck.addChild(headTranslate);
-                    headTranslate.addChild(headRotate);
-                    headRotate.addChild(headScale);
-                      headScale.addChild(headShape);
-                    headRotate.addChild(leftEye);
-                      leftEye.addChild(leftEyeTransform);
-                        leftEyeTransform.addChild(leftEyeShape);
-                    headRotate.addChild(rightEye);
-                      rightEye.addChild(rightEyeTransform);
-                        rightEyeTransform.addChild(rightEyeShape);
-                    headRotate.addChild(rightFeeler);
-                      rightFeeler.addChild(rightFeelerTranslate);
-                        rightFeelerTranslate.addChild(rightFeelerRotate);
-                          rightFeelerRotate.addChild(rightFeelerScale);
-                            rightFeelerScale.addChild(rightArmShape);
-                    headRotate.addChild(leftFeeler);
-                      leftFeeler.addChild(leftFeelerTranslate);
-                        leftFeelerTranslate.addChild(leftFeelerRotate);
-                          leftFeelerRotate.addChild(leftFeelerScale);
-                          leftFeelerScale.addChild(leftFeelerShape);
+  public void pose5(){
+    robot.pose5();
   }
 
   private void egg_scene(GL3 gl){
@@ -612,12 +433,12 @@ public class Museum_GLEventListener implements GLEventListener {
     mobilePhone = initialise_phone(gl,camera,lights,phoneTexture,phoneSpecular);
 //
 //  //Calling the scene functions
-    robot_scene(gl);
+    robot = new Robot(gl, cube,eye,sphere);
     mobile_phone_scene(gl);
     egg_scene(gl);
     lamp_scene(gl);
 
-    robotRoot.update();  // IMPORTANT - don't forget this
+    robot.robotRoot.update();  // IMPORTANT - don't forget this
     pose1();
     phoneRoot.update();
     eggRoot.update();
@@ -641,9 +462,10 @@ public class Museum_GLEventListener implements GLEventListener {
     sidewall.render(gl);
     windowView.render(gl);
 
+    robot.render();
+
 //    if (animation) updateLeftFeeler();
 //    if (animation) updateRightFeeler();
-    robotRoot.draw(gl);
     phoneRoot.draw(gl);
     eggRoot.draw(gl);
     lampRoot.draw(gl);
